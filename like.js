aa405ae072e2474e3df54bfef7dd35c0addd641b
Cristian-Tr <106551922+Cristian-Tr@users.noreply.github.com>
2024-12-24 13:03:27 +0200

    Add files via upload

---
+++ like.js
@@ -0,0 +1,34 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  //Buttons
+  var button = document.querySelector('#like-btn1');
+  var counter = 1;
+  button.addEventListener('click', function () {
+    document.querySelector('#like-btn1 span').innerText = counter;
+    counter++;
+  });
+
+  var button = document.querySelector('#like-btn2');
+  var counter = 1;
+  button.addEventListener('click', function () {
+    document.querySelector('#like-btn2 span').innerText = counter;
+    counter++;
+  });
+
+  var button = document.querySelector('#like-btn3');
+  var counter = 1;
+  button.addEventListener('click', function () {
+    document.querySelector('#like-btn3 span').innerText = counter;
+    counter++;
+  });
+
+  var button = document.querySelector('#like-btn4');
+  var counter = 1;
+  button.addEventListener('click', function () {
+    document.querySelector('#like-btn4 span').innerText = counter;
+    counter++;
+  });
+
+
+}); 
