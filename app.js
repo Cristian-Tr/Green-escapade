4e8df636621a1ddb56566d3925c0aa2bd25978e5
Cristian-Tr <106551922+Cristian-Tr@users.noreply.github.com>
2024-12-31 18:31:52 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,38 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 
+
+  document.getElementById("bookingForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const number = document.getElementById("number").value;
+    const type = document.getElementById("type").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+    const info = document.getElementById("info").value;
+
+    document.getElementById("ticketNumber").textContent = number;
+    document.getElementById("ticketType").textContent = type;
+    document.getElementById("ticketDate").textContent = date;
+    document.getElementById("ticketTime").textContent = time;
+    document.getElementById("ticketInfo").textContent = info;
+
+    document.getElementById("ticket").style.display = "block";
+    document.getElementById("ticket").classList.add("zoomIn");
+});
+
+function printTicket() {
+    const ticket = document.getElementById('ticket');
+    const printWindow = window.open('', '', 'width=600,height=600');
+    printWindow.document.open();
+    printWindow.document.write('<html><head><title>TICKET DETAILS</title></head><body>');
+    printWindow.document.write(ticket.innerHTML);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+    printWindow.close();
+}
+
   //Buttons
   var button = document.querySelector('#like-btn1');
   var counter = 1;
